720ae35d1ebdcbd555abd5853390f6e0bf6320fb
abarkdull <abarkdull@gmail.com>
2021-10-13 15:22:16 -0600

    added dependencies

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.use(express.static("public"));
 
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 3000 at http://localhost:3000");
 });
